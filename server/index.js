8d6a68eb21ef10a0fbe3c82bda6ff009783ff05e
huoqishi <wangchaohua110@sina.com>
2016-11-13 15:40:39 +0800

    tmp

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get('/wechat/index', (req, res) => {
   console.log('开始验证!!!!')
   res.send(req.query.echostr)
 })
-app.post('/wechat/index', (req, res) => {
+app.use('/wechat/index', (req, res) => {
   console.log(req.body)
 })
 
