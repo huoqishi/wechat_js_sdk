93b33061697d5a1245d944b4d4abda687507ae37
huoqishi <wangchaohua110@sina.com>
2016-11-13 16:12:18 +0800

    添加xml2js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+// 转换xmltojs
+const { parseString } = require('xml2js');
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -12,20 +15,24 @@ app.use(express.static('../client'))
 // Get 
 // wechat
 app.get('/wechat/index', (req, res) => {
-  console.log('开始验证!!!!')
-  res.send(req.query.echostr)
+    console.log('开始验证!!!!')
+    res.send(req.query.echostr)
 })
+
 app.use('/wechat/index', (req, res) => {
-  // let dt
-  // req.on('data', data =>{
-  //   dt = data.toString('utf-8')
-  // })
-  // req.on('end', ()=>{
-  //   console.log(dt)
-  // })
-  console.log('有请求过来了')
-  // console.log(req.query)
-  console.log(req.body)
+    // let dt
+    // req.on('data', data =>{
+    //   dt = data.toString('utf-8')
+    // })
+    // req.on('end', ()=>{
+    //   console.log(dt)
+    // })
+    console.log('有请求过来了')
+        // console.log(req.query)
+    console.log(req.body)
+    parseString(req.body, function(err, result) {
+        console.dir(result);
+    });
 })
 
 app.listen(80)
