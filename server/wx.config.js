e846da539ac896a64bcd2d1af56fd7c949eb8b76
huoqishi <wangchaohua110@sina.com>
2016-11-14 06:09:02 +0800

    add

--- server/wx.config.js
+++ server/wx.config.js
@@ -1,6 +1,5 @@
 'use strict'
 // 微信所需要的一些配置
-
 const http = require('http')
 const request = require('request')
 const fs = require('fs')
@@ -10,21 +9,15 @@ const qs = require('querystring');
 const wxConfig = require('./wx.config.json')
 
 const app = {}
-app.APPID = 'wx15abca5df4cf07e9'
-
-// 
-const APPSECRET = 'c65b79d54e6da0667fda63c8f09920a6'
+app.APPID = 'wx15abca5df4cf07e9'                     //AppID(应用ID)
+const APPSECRET = 'c65b79d54e6da0667fda63c8f09920a6' //AppSecret(应用密钥)
 
 // 从微信服务器获取的access_token,每7200秒要重新获取一次
 let Access_Token = wxConfig.access_token
 
-/**
- * 没
- */
+// 从微信服务器获取的jsapi_ticket,每7200秒要重新获取一次
 let Jsapi_Ticket = wxConfig.jsapi_ticket
 
-// 
-// let 
 
 // 1.获取 access_token
 // 备注:ccess_token的有效期目前为2个小时，需定时刷新，重复获取将导致上次获取的access_token失效。
